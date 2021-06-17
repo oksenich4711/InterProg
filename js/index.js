3aa1af5ec053a68005d6520e05f1f2e0c6c8f89d
Kseniia Poliakova <kseniiapoliakova01@gmail.com>
2021-06-17 18:26:41 +0300

    Final fixes done

---
+++ js/index.js
@@ -0,0 +1,45 @@
+var achievementBody = document.querySelector(".achievement-body");
+var achievementWrapper = document.querySelector(".achievement-wrapper");
+
+var btn = document.getElementById("btn");
+
+var COLORS = ["#2196F3", "#A791D4", "#FC7A50", "#5AB65D", "#03A9F4"];
+
+function getColor() {
+  var index = Math.floor(Math.random() * 6);
+
+  return COLORS[index];
+}
+
+function getRandomColor() {
+  var lum = -0.25;
+  var hex = String(
+    "#" +
+      Math.random()
+        .toString(16)
+        .slice(2, 8)
+        .toUpperCase()
+  ).replace(/[^0-9a-f]/gi, "");
+  if (hex.length < 6) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  var rgb = "#",
+    c,
+    i;
+  for (i = 0; i < 3; i++) {
+    c = parseInt(hex.substr(i * 2, 2), 16);
+    c = Math.round(Math.min(Math.max(0, c + c * lum), 255)).toString(16);
+    rgb += ("00" + c).substr(c.length);
+  }
+  return rgb;
+}
+
+const showAchivment = () => {
+  achievementBody.style.backgroundColor = "#0400B3";
+  achievementWrapper.classList.remove("animation");
+  setTimeout(() => achievementWrapper.classList.add("animation"), 200);
+};
+
+document.addEventListener("DOMContentLoaded", showAchivment);
+
+
